4cc238a30aa09d95708d0add8443208f8c2fa351
José Lazaretti <eduardo1793@gmail.com>
2021-10-22 21:00:28 -0300

    Creating model and default values.

---
+++ lib/src/constants/data.dart
@@ -0,0 +1,18 @@
+import 'package:listrandompicker/src/model/item.dart';
+
+final List<Item> defaultItems = <Item>[
+  Item('Alexandre', false),
+  Item('Bijit', false),
+  Item('Christian', false),
+  Item('Daniel', false),
+  Item('Fábio', false),
+  Item('Israel', false),
+  Item('João', false),
+  Item('José', false),
+  Item('Lionel', false),
+  Item('Lucas', false),
+  Item('Luiz', false),
+  Item('Mark', false),
+  Item('Rafael', false),
+  Item('Tim', false),
+];
---
+++ lib/src/model/item.dart
@@ -0,0 +1,6 @@
+class Item {
+  String value;
+  bool picked;
+
+  Item(this.value, this.picked);
+}
